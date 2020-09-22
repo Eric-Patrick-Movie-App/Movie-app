9101af0e7388324fd359b34c88433a5701ab3800
patrick <patricklarkin995@gmail.com>
2020-09-22 15:58:40 -0500

    end of day commit some progress

--- movies.js
+++ movies.js
@@ -3,4 +3,37 @@ $(document).ready(function () {
 	fetch(url)
 			.then(response => response.json())
 			.then(data => console.log(data));
-	})
+
+
+// let addData = new FormData();
+// addData.append("name", $("#movieTitle").val())
+// addData.append("name", $("#movieDate").val())
+// addData.append("name", $("#rating").val())
+
+$('form').submit(function (event) {
+	event.preventDefault();
+
+	let title = $("#movieTitle").val();
+	let date = $("#movieDate").val();
+	let rating = $("#rating").val();
+	console.log(`${title} ${date} ${rating}`);
+})
+
+//		// const reviewObj = {
+// 		// 	restaurant_id: i,
+// 		// 	name: 'Codey',
+// 		// 	rating: 5,
+// 		// 	comments: "This is a really good place for coding and eating"
+// 		// }
+//
+// const url = 'https://codeup-restful-example.glitch.me/reviews';
+// const options = {
+// 	method: 'POST',
+// 	headers: {
+// 		'Content-Type': 'application/json',
+// 	},
+// 	body: JSON.stringify(reviewObj),
+// };
+// fetch(url, options)
+// 		.then( response => console.log(response) ) /* review was created successfully */
+// 		.catch( error => console.error(error) ); /* handle errors */
