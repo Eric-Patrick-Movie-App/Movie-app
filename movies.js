88c209628f3dcbff710b413bad029557ca7f6571
erencehausen <eric.d.rencehausen@gmail.com>
2020-09-23 11:53:50 -0700

    progress on card style

--- movies.js
+++ movies.js
@@ -18,15 +18,15 @@ $(document).ready(function () {
 				let output = ''
 				for (let i = 0; i < data.length; i++) {
 					output += `
-						<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 m-2">
-							<div class="container">
+						<div class="card col-xs-12 col-sm-12 col-md-4 col-lg-3 m-2">
+							<div class="container text-center p-0">
 								<div>
-									<img src="https://placeimg.com/200/250/any" alt="placeholder" class="mt-4">
+									<img src="https://placeimg.com/200/250/any" alt="placeholder" class="my-3">
 								</div>
-								<h3>${data[i].title}</h3>
+								<h3 class="my-2">${data[i].title}</h3>
 								<p>${data[i].year}</p>
-								<p>${data[i].rating}</p>
-								<button type="button" id="remove-btn" class="my-2">Remove</button>
+								<p class="m-0">${data[i].rating}</p>
+								<button type="button" id="remove-btn" class="my-3 py-2 px-3">Remove</button>
 							</div>
 						</div>
 					`
