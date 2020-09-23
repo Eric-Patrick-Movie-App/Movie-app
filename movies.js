d00fefe49464050eef1924d0011cb37aa4d387c7
erencehausen <eric.d.rencehausen@gmail.com>
2020-09-23 09:52:21 -0700

    added movie display functionality

--- movies.js
+++ movies.js
@@ -1,6 +1,37 @@
 $(document).ready(function () {
+
+	const movieList = $('#movie-list');
+
 	const url = 'https://chrome-locrian-pickle.glitch.me/movies';
-	fetch(url)
+	const options = {
+		method: "GET",
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}
+	const getMovie = (url, options) => {
+		fetch(url)
 			.then(response => response.json())
-			.then(data => console.log(data));
+			.catch(error => console.log(error))
+			// .then(data => console.log(data))
+			.then(data => {
+				let output = ''
+				for (let i = 0; i < data.length; i++) {
+					output += `
+						<p>${data[i].id}</p>
+					`
+					movieList.html(output);
+				}
+
+
+			})
+
+	}
+	getMovie(url, options)
+
+
+
+	$(window).on("load", function () {
+		$("#loader-wrapper").fadeOut(5000)
 	})
+})
