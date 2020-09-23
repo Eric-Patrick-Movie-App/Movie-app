038974381e49dc80bd96915eb25836f1d0e2199b
patrick <patricklarkin995@gmail.com>
2020-09-23 10:02:06 -0500

    progress

--- movies.js
+++ movies.js
@@ -4,12 +4,19 @@ $(document).ready(function () {
 			.then(response => response.json())
 			.then(data => console.log(data));
 
+	$("#addMovieBtn").click(function () {
+		getAddMovie();
+		// alert("Success: Added your movie to the list.");
+	});
+
+
 
 // let addData = new FormData();
 // addData.append("name", $("#movieTitle").val())
 // addData.append("name", $("#movieDate").val())
 // addData.append("name", $("#rating").val())
 
+let getAddMovie = function () {
 	$('form').submit(function (event) {
 		event.preventDefault();
 
@@ -17,16 +24,12 @@ $(document).ready(function () {
 		let date = $("#movieDate").val();
 		let rating = $("#rating").val();
 		console.log(`${title} ${date} ${rating}`);
+
+		postMovie(title, date, rating);
 	})
+}
 
-
-// };
-// fetch(url, options)
-// 		.then( response => console.log(response) ) /* review was created successfully */
-// 		.catch( error => console.error(error) ); /* handle errors */
-
-
-// Single iteration card
+// Single iteration movie card
 	let movieCard = function (data) {
 		let movieCard = '';
 		movieCard += `<div class="card bg-secondary text-white p-3">
@@ -37,31 +40,24 @@ $(document).ready(function () {
 		document.getElementById("output").innerHTML = movieCard;
 	}
 
-	$('#addMovieBtn').click(function () {
-		let title = $("#addMovieTitle").val();
-		let date = $("#addMovieDate").val();
-		let rating = $("#addRating").val();
-		let idIteration = 0;
-		postMovie(title, date, rating, idIteration);
-	})
 
-
-	let postMovie = function (data) {
-		const reviewObj = {
-			id: idIteration,
-			name: `${title}`,
-			date: `${date}`,
-			rating: `${rating}`
-		}
-		const options = {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(reviewObj),
-		}
-		fetch(url, options)
-				.then(response => console.log(response)) /* review was created successfully */
-				.catch(error => console.error(error)); /* handle errors */
+//Post the movie to the server
+let postMovie = function (title, date, rating, i = 0) {
+	const reviewObj = {
+		id: i,
+		name: `${title}`,
+		date: `${date}`,
+		rating: `${rating}`
 	}
+	const options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(reviewObj),
+	}
+	fetch(url, options)
+			.then(response => console.log(response)) /* review was created successfully */
+			.catch(error => console.error(error)); /* handle errors */
+}
 })
